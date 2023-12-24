da0ff2c34076b7c02dd1310543b79b29216c1306
BohdanStasov <bstas2612@gmail.com>
2023-12-24 19:08:56 +0200

    context removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { persistor, store } from './redux/store';
 import { Provider } from "react-redux";
-import { StateContext } from './context/StateContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store= {store}>
-      <StateContext>
-        <App />
-      </StateContext>
+      <App />
     </Provider>
   </React.StrictMode>,
 )
